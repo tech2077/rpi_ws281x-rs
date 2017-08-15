e9e9aebbc3cbdb943dc7fb8aa179072eb1426940
Matthew Skolaut <tech2077@gmail.com>
2017-08-15 23:09:04 +0000

    fixes for sys module build

--- rpi_ws281x-sys/build.rs
+++ rpi_ws281x-sys/build.rs
@@ -2,17 +2,19 @@ extern crate gcc;
 
 use std::process::Command;
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
-    let _ = Command::new("git").args(&["clone", "https://github.com/jgarff/rpi_ws281x", env!("OUT_DIR")]).status();
-    gcc:Config::new()
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/mailbox.c"))
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/ws2811.c")),
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/pwm.c")),
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/pcm.c")),
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/dma.c")),
-                .file(concat!(env!("OUT_DIR"), "/rpi_ws281x/rpihw.c"))
-                .include(concat!(env!("OUT_DIR"), "/rpi_ws281x"))
-                .compile("libws2811.a");
+    let out_dir = env::var("OUT_DIR").unwrap().to_string();
+    let _ = Command::new("git").args(&["clone", "https://github.com/jgarff/rpi_ws281x", &format!("{}{}", out_dir, "/rpi_ws281x")]).status();
+    let mut cfg = gcc::Config::new();
+
+
+    cfg.include(format!("{}{}", out_dir, "/rpi_ws281x"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/mailbox.c"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/ws2811.c"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/pwm.c"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/pcm.c"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/dma.c"))
+        .file(format!("{}{}", out_dir, "/rpi_ws281x/rpihw.c"))
+        .compile("libws2811.a");
 }
